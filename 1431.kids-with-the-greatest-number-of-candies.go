308e9993cd391f2a1a4308f96d67275092430b27
T2Knock <huynhtransongngoc@gmail.com>
2026-01-14 08:56:13 +0700

    leet 1431

---
+++ 1431.kids-with-the-greatest-number-of-candies.go
@@ -0,0 +1,17 @@
+// @leet start
+import "slices"
+
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+	result := make([]bool, len(candies))
+	maxVal := slices.Max(candies)
+
+	threshold := maxVal - extraCandies
+
+	for i, candyCount := range candies {
+		result[i] = candyCount >= threshold
+	}
+
+	return result
+}
+
+// @leet end
