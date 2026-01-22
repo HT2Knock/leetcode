b15e171f1669549c92e907bc0edf0945655f5f32
T2Knock <huynhtransongngoc@gmail.com>
2026-01-22 11:48:17 +0700

    leet 2390

---
+++ 2390.removing-stars-from-a-string.go
@@ -0,0 +1,18 @@
+package leetcode
+
+// @leet start
+func removeStars(s string) string {
+	stack := []byte{}
+	for i := 0; i < len(s); i++ {
+		if s[i] == '*' {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+
+	return string(stack)
+}
+
+// @leet end
+
