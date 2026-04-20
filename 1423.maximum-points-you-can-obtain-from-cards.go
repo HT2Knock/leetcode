52fdec407e8d16b7bcf37c11a645f4184283d2c6
T2Knock <huynhtransongngoc@gmail.com>
2026-04-20 10:40:55 +0700

    leet 1423

---
+++ 1423.maximum-points-you-can-obtain-from-cards.go
@@ -0,0 +1,28 @@
+package leetcode
+
+import "slices"
+
+// @leet start
+func maxScore(cardPoints []int, k int) int {
+	allowCards := slices.Concat(cardPoints[len(cardPoints)-k:], cardPoints[0:k])
+	maxSum, windowSum := 0, 0
+	start := 0
+
+	for end := 0; end < len(allowCards); end++ {
+		windowSum += allowCards[end]
+
+		if end-start+1 == k {
+			if windowSum > maxSum {
+				maxSum = windowSum
+			}
+
+			windowSum -= allowCards[start]
+			start++
+		}
+	}
+
+	return maxSum
+}
+
+// @leet end
+
