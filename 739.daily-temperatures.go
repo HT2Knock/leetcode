ef0a42edc49e56f0653c9afaa542425d8fa74858
T2Knock <huynhtransongngoc@gmail.com>
2026-04-23 16:04:32 +0700

    leet 739

---
+++ 739.daily-temperatures.go
@@ -0,0 +1,22 @@
+package leetcode
+
+// @leet start
+func dailyTemperatures(temperatures []int) []int {
+	result := make([]int, len(temperatures))
+	stack := make([]int, 0)
+
+	for i := 0; i < len(temperatures); i++ {
+		for len(stack) > 0 && temperatures[i] > temperatures[stack[len(stack)-1]] {
+			index := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			result[index] = i - index
+		}
+
+		stack = append(stack, i)
+	}
+
+	return result
+}
+
+// @leet end
