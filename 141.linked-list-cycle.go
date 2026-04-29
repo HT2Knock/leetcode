01218a2583c5e21cf5f679868fba187076241e87
T2Knock <huynhtransongngoc@gmail.com>
2026-04-29 10:36:00 +0700

    leet 141 linked list cycle

---
+++ 141.linked-list-cycle.go
@@ -0,0 +1,27 @@
+package leetcode
+
+// @leet start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	fast, slow := head, head
+
+	for fast != nil && fast.Next != nil {
+		if fast == slow {
+			return true
+		}
+
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return false
+}
+
+// @leet end
+
