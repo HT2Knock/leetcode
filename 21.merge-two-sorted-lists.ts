0d02c5c260ab298d61fa7966c34763efcc46504b
T2Knock <huynhtransongngoc@gmail.com>
2026-04-29 16:06:20 +0700

    leet 21: merge two sorted lists

---
+++ 21.merge-two-sorted-lists.ts
@@ -0,0 +1,60 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+// @leet start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null,
+): ListNode | null {
+  if (list1 == null) return list2;
+  if (list2 == null) return list1;
+
+  let head: ListNode | null = null;
+  if (list1.val < list2.val) {
+    head = list1;
+    list1 = list1.next;
+  } else {
+    head = list2;
+    list2 = list2.next;
+  }
+
+  let current = head;
+  while (list1 != null && list2 != null) {
+    if (list1?.val < list2?.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+
+    current = current.next;
+  }
+
+  if (list1 != null) {
+    current.next = list1;
+  } else {
+    current.next = list2;
+  }
+
+  return head;
+}
+// @leet end
+
