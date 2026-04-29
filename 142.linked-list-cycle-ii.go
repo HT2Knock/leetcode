a9346b6d02b93e7702f125ad219893f9a95c5cea
T2Knock <huynhtransongngoc@gmail.com>
2026-04-29 11:35:28 +0700

    leet 142 detect cycle ii

---
+++ 142.linked-list-cycle-ii.go
@@ -0,0 +1,35 @@
+package leetcode
+
+// @leet start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	fast := head
+	slow := head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+
+		if fast == slow {
+			slow = head
+
+			for slow != fast {
+				slow = slow.Next
+				fast = fast.Next
+			}
+
+			return slow
+		}
+	}
+
+	return nil
+}
+
+// @leet end
+
