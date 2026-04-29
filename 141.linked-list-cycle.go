ba57c3cbf810b6fd31624591a4d5fdb97b64ec67
T2Knock <huynhtransongngoc@gmail.com>
2026-04-29 10:44:40 +0700

    fix: leet 141

--- 141.linked-list-cycle.go
+++ 141.linked-list-cycle.go
@@ -12,12 +12,13 @@ func hasCycle(head *ListNode) bool {
 	fast, slow := head, head
 
 	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+
 		if fast == slow {
 			return true
 		}
 
-		fast = fast.Next.Next
-		slow = slow.Next
 	}
 
 	return false
