69508aeb49f4d8635230c07b34babb9dc752901d
T2Knock <huynhtransongngoc@gmail.com>
2026-05-15 11:58:07 +0700

    leet 112: path sum easy

---
+++ 112.path-sum.go
@@ -0,0 +1,26 @@
+package leetcode
+
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	target := targetSum - root.Val
+	if target == 0 && root.Left == nil && root.Right == nil {
+		return true
+	}
+
+	return hasPathSum(root.Left, target) || hasPathSum(root.Right, target)
+}
+
+// @leet end
+
